3aff3876d2ff52d1429cab71c93018028ed6a1ce
ekrem0691 <06hilalekrem06@gmail.com>
2022-08-11 01:49:09 +0300

    events added

--- src/components/3-mouseEvents/MouseEvent.jsx
+++ src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,29 +1,23 @@
-
-import { useState } from 'react';
-import { FaReact } from 'react-icons/fa';
-import { FaEvernote } from 'react-icons/fa';
+import { useState } from "react";
+import { FaReact } from "react-icons/fa";
+import { FaEvernote } from "react-icons/fa";
 
 const MouseEvent = () => {
-  
-
   const [visible, setVisible] = useState(false);
   const [toggleBg, setToggleBg] = useState(false);
 
-  
   const [coordX, setCoordX] = useState();
   const [coordY, setCoordY] = useState();
 
-
-
-
-  const handleDoubleClick = (e)=>{
+  const handleDoubleClick = (e) => {
     setToggleBg(true);
-    toggleBg ? (e.target.className = 'bg-danger text-light w-50 p-4 mt-4') : (e.target.className = 'bg-success text-light w-50 p-4 mt-4')
-  }
+    toggleBg
+      ? (e.target.className = "bg-danger text-light w-50 p-4 mt-4")
+      : (e.target.className = "bg-success text-light w-50 p-4 mt-4");
+  };
 
-
-   //? mouseMove event
-   const handleMouseMove = (e) => {
+  //? mouseMove event
+  const handleMouseMove = (e) => {
     //! Goreceli koordinatlar
     // setCoordX(e.nativeEvent.offsetX);
     // setCoordY(e.nativeEvent.offsetY);
@@ -33,9 +27,6 @@ const MouseEvent = () => {
     setCoordY(e.pageY);
   };
 
- 
-    
-
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
       <h2 className="text-danger">MOUSE EVENTS</h2>
@@ -43,9 +34,8 @@ const MouseEvent = () => {
       <div
         id="todo-1"
         className="bg-success text-light w-50 p-4"
-        onMouseEnter={()=>setVisible(true)}
-        onMouseLeave = {()=>setVisible(false)}
-       
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
       >
         todo item 1 <FaReact />
       </div>
@@ -54,9 +44,7 @@ const MouseEvent = () => {
       <div
         id="todo-2"
         className="bg-success text-light w-50 p-4 mt-4"
-        onDoubleClick = {handleDoubleClick}
-  
-      
+        onDoubleClick={handleDoubleClick}
       >
         todo item 2 <FaEvernote />
       </div>
@@ -65,16 +53,13 @@ const MouseEvent = () => {
         id="todo-3"
         className="bg-success text-light w-50 p-4 my-4"
         onMouseMove={handleMouseMove}
-        
       >
         todo item 3 <FaReact />
       </div>
 
       <p>X and Y</p>
       <p className="text-danger fw-bold">
-
-      {coordX} {coordY}
-     
+        {coordX} {coordY}
       </p>
     </div>
   );
